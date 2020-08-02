10a5a34ab5de156e2a4e330adcdc5f7f5ab834e6
linhuiliu <1249055292@qq.com>
2020-08-03 00:10:19 +0800

    init

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    abi: require('./BatchExchange.json'),
+    endpoint: 'https://mainnet.infura.io/I1bDSFCAZW1DvGWDKdLy',
+    privateKey: 'ca29d3de0dec8a67153c72b7a6939c16f45005e090acf9f1ea6c81b155b4009b',
+    contractAddress: '0x4F0B09Be4032772FF023Ab374b45c615FfD3F4e5',
+    address: '0xE9C6F61b80505b211027955Df0D45F9C12009da4',
+}
